6d1d6223b1295684f87360a8d188feedebac6641
Robert Lester <robertlesterjr@mac.com>
2022-02-13 21:41:51 -0500

    version patch.

--- dist/engagement.js
+++ dist/engagement.js
@@ -1,4 +1,4 @@
-/*! LivePerson Engagement Version 2.0.0 */
+/*! LivePerson Engagement Version 2.0.1 */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
